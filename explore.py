d3b4436ebcd1d7a6bed7c8e66e8a85b9586ee761
Geoffrey Liu <liug.2012@gmail.com>
2014-11-06 00:53:52 -0800

    Initial commit

---
+++ explore.py
@@ -0,0 +1,7 @@
+# On several occasions, I have begun typing on my keyboard in one layout when I thought it was in another.
+# As a double QWERTY/Dvorak user, this often results in meaningless strings "pgvd kjg;e"
+# But now I wonder whether there are such pairs of words whose keystroke patterns are shared between
+# QWERTY and DVORAK...
+
+# Run this by passing in a dictionary.txt file in the command line args.
+
