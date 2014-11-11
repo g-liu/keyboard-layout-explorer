e5e687e4071e3a738f45e2e3dd80d9603fca25ac
Geoffrey Liu <liug.2012@gmail.com>
2014-11-10 18:50:05 -0800

    initial commit of analysis script

---
+++ matchAnalyze.py
@@ -0,0 +1,7 @@
+# Analyze the matches for certain patterns
+# Use as: py matchAnalyze.py < match_file
+#
+# Each line of the match file should consist of two strings separated by a tab space.
+# The two strings correspond to the mapping from keystrokes in one layout to keystrokes
+# in another layout.
+
